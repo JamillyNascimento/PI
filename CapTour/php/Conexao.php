6211235f52bda28a77a69b8e6d25c68c0e49d34e
Jamilly Nascimento <81826976+JamillyNascimento@users.noreply.github.com>
2022-11-28 11:44:26 -0300

    Update Conexao.php

--- CapTour/php/Conexao.php
+++ CapTour/php/Conexao.php
@@ -4,6 +4,15 @@
 	$senha = '';
 	$banco = 'CapTour';
 
+	$server_url = $_SERVER['SERVER_NAME'];
+
+	
+	if ($server_url !== 'localhost') {
+		$servidor = 'sql313.epizy.com';
+		$usuario = 'epiz_33092822';
+		$senha = 'Eo7LUxpyjx';
+		$banco = 'epiz_33092822_captour';
+	}
 	$cn = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 ?>
