3c15b677ac1c18b789e64a6c1f143a0a709500c5
Jamilly Nascimento <81826976+JamillyNascimento@users.noreply.github.com>
2022-12-01 07:18:10 -0300

    Update VerificaCadastro.php

--- CapTour/php/VerificaCadastro.php
+++ CapTour/php/VerificaCadastro.php
@@ -1,27 +1,24 @@
 <?php 
-	
-	include ('Conexao.php');
-
-	$Nome = $_POST['Nome'];
-	$Email = $_POST['Email'];
-	$Senha = $_POST['Senha'];
-	$Status = $_POST['Status'];
-
+    
+    include ('Conexao.php');
+ 
+    $Nome = $_POST['Nome'];
+    $Email = $_POST['Email'];
+    $Senha = $_POST['Senha'];
+    $Status = $_POST['Status'];
+ 
     $consulta = mysqli_query($cn, "SELECT * FROM `cadastro` WHERE `EMAIL` = '$Email'");
     $exibe = mysqli_fetch_all($consulta, MYSQLI_ASSOC);
     print_r($exibe);
-	if (count($exibe) >= 1) {
-		//echo "usuário já cadastrado";
-		echo "<script>alert('Usuário já existente!')</script>";
-	} else{
-		//echo "Usuário NÃO cadastrado";
-    $sql = "INSERT INTO `cadastro`(`NOME`, `EMAIL`, `SENHA`) values ('$Nome', '$Email', '$Senha')";
-		$incluir = $cn->query($sql);
-		echo "<script>alert('Usuário cadastrado!')</script>";
-		header('location:../Index.php');
-	}
-
+    if (count($exibe) >= 1) {
+        //echo "usuário já cadastrado";
+        echo "<script>alert('Usuário já existente!')</script>";
+    } else{
+        //echo "Usuário NÃO cadastrado";
+    $sql = "INSERT INTO `cadastro`(`NOME`, `EMAIL`, `SENHA`, `STATUS`) values ('$Nome', '$Email', '$Senha', '$Status')";
+        $incluir = $cn->query($sql);
+        echo "<script>alert('Usuário cadastrado!')</script>";
+        header('location:../Index.php');
+    }
+ 
 ?>
-
-
-
